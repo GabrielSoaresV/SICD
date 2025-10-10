816e0a1fe9f2ef51dc2cfddb07c9096598ef8fac
Gabriel Soares <122445286+GabrielSoaresV@users.noreply.github.com>
2025-10-10 12:46:58 -0300

    Delete avaliacao-backend directory

--- avaliacao-backend/src/main/java/com/projetoavaliacao/repository/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoavaliacao.repository;
-
-import com.projetoavaliacao.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, String> {
-}
--- avaliacao-backend/src/main/java/com/projetoavaliacao/repository/JovemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoavaliacao.repository;
-
-import com.projetoavaliacao.model.JovemAprendiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JovemRepository extends JpaRepository<JovemAprendiz, String> {}
-
